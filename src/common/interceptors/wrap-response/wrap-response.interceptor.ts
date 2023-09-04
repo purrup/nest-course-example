3e93eb981244add8cc1009c063e71a2265f850a9
Vin <purrup27@gmail.com>
2023-09-04 16:48:55 +0800

    add timeout interceptor

--- src/common/interceptors/wrap-response/wrap-response.interceptor.ts
+++ src/common/interceptors/wrap-response/wrap-response.interceptor.ts
@@ -10,8 +10,6 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class WrapResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
-
     return next.handle().pipe(map((data) => ({ data })));
   }
 }
