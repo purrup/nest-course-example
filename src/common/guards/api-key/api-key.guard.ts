6957ee68aae0f31ef04be57e01c7c6d56624c19e
Vin <purrup27@gmail.com>
2023-09-04 16:24:19 +0800

    add public decorator

--- src/common/guards/api-key/api-key.guard.ts
+++ src/common/guards/api-key/api-key.guard.ts
@@ -1,14 +1,28 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from 'src/common/decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get<boolean>(
+      IS_PUBLIC_KEY,
+      context.getHandler(),
+    );
+    if (!isPublic) {
+      return false;
+    }
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Authorization');
-    return authHeader === process.env.API_KEY;
+    return authHeader === this.configService.get('API_KEY');
   }
 }
